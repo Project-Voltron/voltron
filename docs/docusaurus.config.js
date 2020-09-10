10b62c0f238d1dd6e9fef42506cc34c884f22511
jinoojain <jinoo.jain@gmail.com>
2020-09-10 16:16:16 -0600

    bug fix: clicking "Edit this page" now works

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -69,7 +69,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/Project-Voltron/voltron',
+            'https://github.com/Project-Voltron/voltron/tree/master/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
